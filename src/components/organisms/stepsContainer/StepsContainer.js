297f577142f34347127afdc18c9921d310d729a2
lorenacostace <lorenacostace@gmail.com>
2021-07-30 21:04:54 +0200

    style: Added styles to the StepContainer component

--- src/components/organisms/stepsContainer/StepsContainer.js
+++ src/components/organisms/stepsContainer/StepsContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './StepsContainer.css'
+import { Container } from "./StepsContainerStyle";
 
 function StepsContainer (props) {
 
     return (
-        <div>
+        <Container>
             { props.steps.map((step, index) => (
                 <div key={index} className={ props.currentStep !== index ? 'stepHidden': '' }>
                     { step.component }
                 </div>
             ))}
-        </div>
+        </Container>
     )
 }
 
